81551d4c8fcebaed37aeeda97bcab85ffb26497c
Luca Tassinari <luca.tassinari.2000@gmail.com>
2025-11-05 17:00:00 +0100

    improvements in 02 and init 03 structure

--- resources/code/Gradient.scala
+++ resources/code/Gradient.scala
@@ -20,7 +20,8 @@ object Gradient:
   def gradient(using Lang): Double =
     share(Double.MaxValue): nvalues =>
       val distances = senseDistance[Double]
-      val minDistance = (nvalues, distances).map2(_ + _)
+      val minDistance = (nvalues, distances)
+        .map2(_ + _)
         .withoutSelf.min
       if isSource then 0.0 else minDistance
 
